3d1050b31f90a96f646913b02bfcabd56cca86a2
Jean$ <jeancarlossoares.s@gmail.com>
2022-03-23 00:15:25 -0300

    First commit - Project created

---
+++ src/main/java/com/lostline/webservices_nosql/WebServicesNoSqlApplication.java
@@ -0,0 +1,13 @@
+package com.lostline.webservices_nosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesNoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesNoSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lostline/webservices_nosql/WebServicesNoSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lostline.webservices_nosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesNoSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
