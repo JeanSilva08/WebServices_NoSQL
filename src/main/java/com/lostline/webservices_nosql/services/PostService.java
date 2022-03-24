e4e7f7963998318ad9cdb1f073921ffc0f307173
Jean$ <jeancarlossoares.s@gmail.com>
2022-03-24 18:23:22 -0300

    Getting post by Id

---
+++ src/main/java/com/lostline/webservices_nosql/services/PostService.java
@@ -0,0 +1,25 @@
+package com.lostline.webservices_nosql.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lostline.webservices_nosql.domain.Post;
+import com.lostline.webservices_nosql.repository.PostRepository;
+import com.lostline.webservices_nosql.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
+}
