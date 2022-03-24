352aa6197c14a66800d807b459143b32ccf7aff9
Jean$ <jeancarlossoares.s@gmail.com>
2022-03-24 17:57:46 -0300

    Endpoint returning users post

--- src/main/java/com/lostline/webservices_nosql/resources/UserResource.java
+++ src/main/java/com/lostline/webservices_nosql/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.lostline.webservices_nosql.domain.Post;
 import com.lostline.webservices_nosql.domain.User;
 import com.lostline.webservices_nosql.dto.UserDTO;
 import com.lostline.webservices_nosql.services.UserService;
@@ -64,5 +65,13 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+
 	}
 }
